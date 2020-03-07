bf475bada671093c6d446361e839ea0304050617
antoniocarlosnieto <antoniocarlosnieto@gmail.com>
2020-03-07 10:58:59 -0300

    criação do projeto

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World2");
+        System.out.println("Hello World3");
+        System.out.println("Hello World4");
+
+    }
+}
