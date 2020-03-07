86acf61041ba37b0b0395775e9cb450eba3c5d20
antoniocarlosnieto <antoniocarlosnieto@gmail.com>
2020-03-07 17:34:16 -0300

    treinamento de alterações

--- src/Exercicio3/AplicacaoNotas.java
+++ src/Exercicio3/AplicacaoNotas.java
@@ -1,10 +1,21 @@
 package Exercicio3;
 
+//Criação da Classe
 public class AplicacaoNotas {
+    //Criação da Classe Main
     public static void main(String[] args) {
+        //Criando o novo Objeto
         GerenciaNotas aluno = new GerenciaNotas("Ricardo", 7);
+        //Validando o Status do Aluno chamando o Método
         boolean nota = aluno.aprovado();
-        System.out.println("O aluno "+aluno.nomeAluno +" com nota final " + aluno.nota + " esta "+nota);
+        //If para frase correta.
+          if (nota) {
+            // Se Aprovado
+            System.out.println("O aluno " + aluno.nomeAluno + " com nota final " + aluno.nota + " esta aprovado");
+        } else {
+            // Se Reprovado
+            System.out.println("O aluno "+aluno.nomeAluno +" com nota final " + aluno.nota + " esta reprovado");
+        }
 
     }
 
